93f55a84f87383988f222d66fc5cc0256cdc17de
Ethan Romm <ejromm@gmail.com>
2023-06-28 17:40:22 -0400

    added fakestore api call to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,38 @@ import { Container } from "react-bootstrap"
 import Home from "./pages/Home"; 
 import Store from "./pages/Store";
 import NavBar from "./components/NavBar";
+import { useState, useEffect, SetStateAction } from "react";
+type RateItem = {
+    rate: number;
+    count: number;
+}
+interface StoreItem  {
+     category: string, 
+     description: string,
+     id: number, 
+     price: number,
+     rating: RateItem,
+     title: string,
+}
 function App() {
-  
+  const [data, setData] = useState([] as StoreItem[]);
+
+  useEffect(() => {
+      async function ApiCall(): Promise<SetStateAction<StoreItem[]> | undefined>{
+          try {
+              const response = await fetch('https://fakestoreapi.com/products'); 
+              const data = await response.json(); 
+              console.log(data);
+              setData(data);
+              return data;
+          } catch(err) {
+              console.error(err);
+            
+          }
+      }
+      ApiCall();
+      
+  },[])
 
   return (
     <>
