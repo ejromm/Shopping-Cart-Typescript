fbd86ff72fc7dd1f43f72495ad10bc2d97ad1e31
Ethan Romm <ejromm@gmail.com>
2023-07-01 00:50:43 -0400

    styled cart item quantity inputs

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -13,20 +13,19 @@ export function CartItems({item, data}: CartItemProps) {
     if(cItem == null) return null; 
     return (
         <Stack direction='horizontal' gap={2} className='d-flex align-items-center'>
-        <Image src={cItem.image} style={{ border: '1px solid black', width: "15%", maxHeight: "100px", objectFit:'contain'}} />
-        <div style={{border: '1px solid black'}} className='me-auto'>
-            <div style={{fontSize: '.8rem'}}>{cItem.title}{" "}</div>  
+        <Image src={cItem.image} style={{ border: '1px solid black', width: "20%", maxHeight: "150px", objectFit:'contain'}} />
+        <div style={{maxWidth:'50%'}} className='me-auto'>
+            <div style={{fontSize: '.8rem', }}>{cItem.title}{" "}</div>  
             <div className="text-muted" style={{ fontSize: '.7rem'}}>{formatCurrency(cItem.price)}</div>   
             
         </div>
         <div>
-            <input type="button" value="-" onClick={item.quantity > 1 ? () =>decreaseCartQuantity(item.id) : () => removeFromCart(item.id)} />
-             <input type="number"
+            <input className='bg-white border-0' type="button" value="-" onClick={item.quantity > 1 ? () =>decreaseCartQuantity(item.id) : () => removeFromCart(item.id)} />
+             <input  style={{ width:'25%', fontSize: '.7rem', textAlign: 'center'}} type="number"
              step="1"
-             max=""
              value={item.quantity}
               />
-            <input type="button" value="+"  onClick={() => increaseCartQuantity(item.id)} />
+            <input className='bg-white border-0' type="button" value="+"  onClick={() => increaseCartQuantity(item.id)} />
         </div>
         </Stack>
     )
