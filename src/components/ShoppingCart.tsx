04fcec0ed471f95225966f35901d4438d901f017
Ethan Romm <ejromm@gmail.com>
2023-06-30 23:18:19 -0400

    added responsive cart width

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -4,6 +4,7 @@ import { CartItem } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { StoreItem } from "../App";
+import { useState, useEffect } from "react";
 // type for checking if shopping cart modal is set to open
 type ShoppingCartProps = { 
     isOpen: boolean
@@ -11,9 +12,21 @@ type ShoppingCartProps = {
 };
 export function ShoppingCart({isOpen, data}: ShoppingCartProps) {
     const { closeCart, cartItems } = useShoppingCart();
+    const [matches, setMatches] = useState(
+        window.matchMedia("(max-width: 992px)").matches
+      )
+    
+      useEffect(() => {
+        window
+        .matchMedia("(max-width: 992px)")
+        .addEventListener('change', e => setMatches( e.matches ));
+      }, []);
+    
+    console.log(matches);
+      
     return (
-        <Offcanvas show={isOpen} onHide={closeCart} placement="end" >
-        <Offcanvas.Header><Offcanvas.Title>Your Shopping Bag</Offcanvas.Title></Offcanvas.Header>
+        <Offcanvas style={{width: matches ? '100%': '30%'}}  show={isOpen} onHide={closeCart} placement="end" >
+        <Offcanvas.Header closeButton><Offcanvas.Title>Your Shopping Bag</Offcanvas.Title></Offcanvas.Header>
         <Offcanvas.Body>
             <Stack gap={3}>
             {cartItems.map((item: CartItem) => (
@@ -29,3 +42,5 @@ export function ShoppingCart({isOpen, data}: ShoppingCartProps) {
     )
 
 }
+
+
