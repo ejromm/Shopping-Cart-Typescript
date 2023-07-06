1f169c3cebd4c465bd82a7559bbdf70861b97130
Ethan Romm <ejromm@gmail.com>
2023-07-05 21:05:38 -0400

    fully responsive header

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,20 +6,40 @@ import { useState, useEffect } from "react";
 export default function NavBar() {
     const {openCart} = useShoppingCart();
     const [hamburgerOpen, setHamburgerOpen] = useState(false);
+    const [isLarge, setIsLarge] = useState(window.matchMedia("(min-width: 768px)").matches); 
     useEffect(() => {
+        console.log('large?',isLarge);
+        window
+        .matchMedia("(min-width: 768px)")
+        .addEventListener('change', (e) => {
+            if(e.matches) {
+                setIsLarge(true); 
+                
+            }
+             else {
+                setIsLarge(false)
+             }  
+            
+                
+            
+                
+        });
         hamburgerOpen ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'scroll'; 
-     }, [hamburgerOpen])
+        console.log('large?',isLarge);
+        console.log('hamburger', hamburgerOpen)
+      }, [isLarge, hamburgerOpen]);
+   
     // return bootstrap navbar with links to pages
     return( 
-        <NavbarBs collapseOnSelect expand='md' sticky="top" className="bg-black d-flex align-items-start" style={{height: '10vh', paddingTop: '0'}} variant="dark">
+        <NavbarBs  collapseOnSelect expand='md' sticky="top" className="bg-black d-flex align-items-start" style={{height: '10vh', paddingTop: '0'}} variant="dark">
             <Container fluid className="d-flex flex-row align-items-center p-0" style={{ height: '10vh'}} >
-                <NavbarBs.Brand className="text-white d-flex align-items-center" href='/' style={{fontFamily: "Times New Roman", fontSize: '3rem', letterSpacing: '.3vw', height:'10vh', marginLeft: '5%'}}>FAKE STORE</NavbarBs.Brand>
+                <NavbarBs.Brand className="text-white d-flex align-items-center" href='/' style={{fontFamily: "Times New Roman", fontSize: 'clamp(2rem, 3vw,3rem)', letterSpacing: '.3vw', height:'10vh', marginLeft: '5%'}}>FAKE STORE</NavbarBs.Brand>
                 <NavbarBs.Toggle onClick={() => setHamburgerOpen(!hamburgerOpen)} aria-controls="responsive-navbar-nav" />
-                <NavbarBs.Collapse  style={{ transition: 'none'}}  className="justify-content-end w-100"   id="responsive-navbar-nav">
-                <Nav  className="gap-10%" style={{ fontFamily:'Roboto, sans-serif', fontSize:'1.2rem',  backgroundColor: hamburgerOpen ?  'rgb(0,0,0,.75)' : '', width: hamburgerOpen ? '100vw': '', height: hamburgerOpen ? '90vh' : '',  marginRight: '5%', alignItems: 'center', justifyContent: 'center'}} >
-                    <Nav.Link className="text-white" to='/' as={NavLink}>Home</Nav.Link>
-                    <Nav.Link className="text-white" to='/store' as={NavLink}>Shop</Nav.Link>
-                    <Nav.Link onClick={openCart} className="text-white" eventKey=''>Cart</Nav.Link>
+                <NavbarBs.Collapse  style={{  transition: 'none'}}  className="justify-content-end w-100"   id="responsive-navbar-nav">
+                <Nav  className="gap-10%" style={{ fontFamily:'Roboto, sans-serif', fontSize:'1.2rem', backgroundColor: !isLarge && hamburgerOpen ?  'rgb(0,0,0,.75)' : '', width: !isLarge && hamburgerOpen ? '100vw' : '', height: !isLarge && hamburgerOpen ? '90vh' : '',  marginRight: '5%', alignItems: 'center', justifyContent: 'center'}} >
+                    <Nav.Link onClick={() => setHamburgerOpen(!hamburgerOpen)} className="text-white" to='/' as={NavLink}>Home</Nav.Link>
+                    <Nav.Link onClick={() => setHamburgerOpen(!hamburgerOpen)} className="text-white" to='/store' as={NavLink}>Shop</Nav.Link>
+                    <Nav.Link onClick={() => {setHamburgerOpen(!hamburgerOpen); openCart();}} className="text-white" eventKey=''>Cart</Nav.Link>
                 </Nav>
                 </NavbarBs.Collapse>
             </Container>
